58e11022358f85a7b87dd32592c2e81516eb99b1
manuelbanderada <manuelbanderada@gmail.com>
2023-03-22 13:34:33 +0800

    task: exercise5

--- exercise-5/src/app/boot.ts
+++ exercise-5/src/app/boot.ts
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
-import todo from "../app/redux/todo/reducers";
-import task from "../app/redux/task/slice";
+import todo from "../app/redux/todo/task/task.reducers";
+import task from "../app/redux/task/task.slice";
 
 export const store = createStore(
     combineReducers({
